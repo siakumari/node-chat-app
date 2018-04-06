2b0b54630c64e5423b9f7d419d7dec70992ed1f4
shalini kumari <shalinikumari2598@gmail.com>
2018-04-06 23:34:19 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,13 @@
 var socket = io();
 
-socket.on('connect',function (){
-  console.log('connected to server');
-
-  // socket.emit('createEmail',{
-  //   to:"jen@example.com",
-  //   text:'hey.this is dean.'
-  // });
-
-  socket.emit('createMessage',{
-    from:'shalini',
-    text:'yup,that works for me.'
-  });
+socket.on('connect', function () {
+  console.log('Connected to server');
 });
 
-socket.on('disconnect',function (){
-  console.log('Disconnected fro the server');
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
 });
 
-// socket.on('newEmail', function(email){
-//   console.log('New email',email);
-// });
-
-
-socket.on('newMessage',function(Message){
-  console.log('newMessage',Message);
+socket.on('newMessage', function (message) {
+  console.log('newMessage',message);
 });
