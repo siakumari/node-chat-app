05343c2dfc458801ba986fa4d8690a1376f35fee
shalini kumari <shalinikumari2598@gmail.com>
2018-04-07 14:48:49 +0530

    create getMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -11,45 +12,27 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection' , (socket)=>{
-  console.log('New User connected');
+io.on('connection', (socket) => {
+  console.log('New user connected');
 
-socket.emit('newMessage',{
-  from:'Admin',
-  text:'Welcome to chat app',
-  createdAt:new Date().getTime()
-});
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-socket.broadcast.emit('newMessage',{
-  from:'Admin',
-  text:'New user joined',
-  createdAt:new Date().getTime()
-});
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+  });
 
-   socket.on('createMessage',(message)=>{
-     console.log('createMessage',message);
-     // io.emit('newMessage',{
-     //   from:message.from,
-     //   text:message.text,
-     //   createdAt:new Date().getTime()
-     // });
-
-     // socket.broadcast.emit('newMessage',{
-     //   from:message.from,
-     //   text:message.text,
-     //   createdAt:new Date().getTime()
-     // });
-
-   });
-
-
-  socket.on('disconnect',()=>{
+  socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
 });
-
-
 
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
